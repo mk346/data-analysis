f561d488868d766c817555dafd584d6789c24d86
mk346 <calebkinyuamunene@gmail.com>
2022-11-24 20:48:34 +0300

    #deploy

--- task3_2.py
+++ task3_2.py
@@ -5,33 +5,31 @@
 # importing the necessary libraries
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import re
+import numpy as np
 
 # reading the training and testing dataset csv files
 training = pd.read_csv("training.csv")
-training_data = pd.DataFrame(training)
-training_data['TotalCharges'] = training_data['TotalCharges'].str.strip()
-training_data2 = re.findall(r'\d+\.\d+', training_data)
-print(training_data.dtypes)
-# testing = pd.read_csv("testing.csv")
-# test_label = pd.read_csv("test_label.csv")
-#dataframe['quantity'] = dataframe['quantity'].astype(float)
-#training_data["TotalCharges"] = training_data["TotalCharges"].astype(str).astype(float)
-# training['TotalCharges'].strip(" ")
+testing = pd.read_csv("testing.csv")
+test_label = pd.read_csv("test_label.csv")
 
-# X_train = training[['TotalCharges']]
-# y_train = training[['Churn']]
-# training.strip('')
+#converting the totalcharges column from object dtype to float
+training["TotalCharges"] = pd.to_numeric(training["TotalCharges"], errors='coerce')
+
+# dropping the NaN values in the dataset
+training_data= training.dropna(axis= 0, how='any')
+
+# assigning x_train and y_train 
+X_train = training_data[['TotalCharges']]
+y_train = training_data[['Churn']]
 
 
-# print(X_train.dtypes)
 
 # training the model
-# regressor = LinearRegression(fit_intercept=True)
-# regressor.fit(X_train, y_train)
+regressor = LinearRegression(fit_intercept=True)
+regressor.fit(X_train, y_train)
 
-# # print the slope 
-# print('Slope of the Line (m)', regressor.coef_)
+# print the slope 
+print('Slope of the Line (m)', regressor.coef_) # note the slope is negative hence totalcharges and churn have an inverse relationship
 
-# # print the gradient
-# print('Gradient of the line (x)', regressor.intercept_)
+# print the gradient
+print('Gradient of the line (x)', regressor.intercept_)
